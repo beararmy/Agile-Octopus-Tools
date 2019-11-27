48cd8466e6676f7d6f1dcd6402645f418ea07941
Stef <stef@localhost.localdomain>
2019-11-27 14:56:15 +0000

    created TestOctopusLogin

--- functions.php
+++ functions.php
@@ -1,2 +1,22 @@
 <?php
 require './secrets.php'; # DB Credentials and Spotify API creds
+
+function TestOctopusLogin($api_key, $call_url)
+{
+    try {
+        $handle = curl_init($call_url);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, TRUE);
+        $response = curl_exec($handle);
+        $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+        if ($httpCode != 200) {
+            throw new Exception("Got a non-200 response");
+        }
+        curl_close($handle);
+        $status_octopus = "true";
+    } catch (\Throwable $th) {
+        $status_octopus = "false";
+        throw new Exception("API Connection Failed");
+    } finally {
+        return $status_octopus;
+    }
+}
