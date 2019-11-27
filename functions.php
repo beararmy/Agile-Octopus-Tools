e0945f5070670ce381773a1c38e252b2e5d9737d
Stef <stef@localhost.localdomain>
2019-11-27 14:57:45 +0000

    Added TestMySQLLogin

--- functions.php
+++ functions.php
@@ -20,3 +20,24 @@ function TestOctopusLogin($api_key, $call_url)
         return $status_octopus;
     }
 }
+function TestMySQLLogin()
+{
+    try {
+        require './secrets.php'; # DB Credentials and Spotify API creds
+        $conn = new mysqli($db_servername_8459, $db_username_2734, $db_password_1924, $db_name_9781) or die("Unable to Connect");
+        $sql = "SELECT * from $db_tablename_9834 LIMIT 1;";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $status_octopus = "true";
+        } else {
+            $status_octopus = "false";
+            throw new Exception("MySQL Connection Failed");
+        }
+        $conn->close();
+    } catch (\Throwable $th) {
+        $status_octopus = "false";
+        throw new Exception("MySQL Connection Failed");
+    } finally {
+        return $status_octopus;
+    }
+}
