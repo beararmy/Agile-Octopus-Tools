cfb124c0b6ba45cca599a46f5968a55589ce0a19
Stef <stef@localhost.localdomain>
2019-11-27 14:54:18 +0000

    secrets default config

---
+++ secrets.DEFAULTS.php
@@ -0,0 +1,19 @@
+<?php
+
+# Replace the x's with your own config
+
+$db_servername_8459 = "x";
+$db_username_2734   = "x";
+$db_password_1924   = "x";
+$db_name_9781       = "x";
+$db_tablename_9834  = "ElectricPrices";
+$db_tablename_9833  = "ElectricConsumption";
+
+$api_key                = "x";
+$elec_mpan              = "x";
+$elec_serial            = "x";
+$base                   = "https://api.octopus.energy/v1/";
+$products               = "products/";
+$emeter                 = "electricity-meter-points/";
+$upcomingagilerate      = "products/AGILE-18-02-21/electricity-tariffs/E-1R-AGILE-18-02-21-A/standard-unit-rates/";
+$agile_reset_time_utc0  = "16:00";
