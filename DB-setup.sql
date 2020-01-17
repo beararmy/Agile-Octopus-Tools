7d3a4b4c40a5ccbc6dd1ff5336611b8d9ee93857
beararmy <stef@bear.army>
2020-01-17 10:43:09 +0000

    documentation refresh

--- DB-setup.sql
+++ DB-setup.sql
@@ -1,27 +1,34 @@
 CREATE DATABASE `AgileOctopus`;
-CREATE USER 'OctopusUser'@'localhost' IDENTIFIED BY 'xxxxA password better than this.xxxx';
-GRANT ALL PRIVILEGES ON AgileOctopus.*  TO 'OctopusUser'@'localhost';
+
+CREATE USER 'OctopusUser' @'localhost' IDENTIFIED BY 'xxxxA password better than this.xxxx';
+
+GRANT ALL PRIVILEGES ON AgileOctopus.* TO 'OctopusUser' @'localhost';
+
 FLUSH PRIVILEGES;
+
 USE 'AgileOctopus';
+
 CREATE TABLE `ElectricPrices` (
-        `entry_id` INT NOT NULL AUTO_INCREMENT,
-        `valid_from` DATETIME NOT NULL COMMENT 'UTC+0',
-        `valid_to` DATETIME NOT NULL COMMENT 'UTC+0',
-        `value_exc_vat` DECIMAL(7,4) NOT NULL COMMENT 'GBp',
-        `value_inc_vat` DECIMAL(7,4) NOT NULL COMMENT 'GBp',
-        PRIMARY KEY (`entry_id`)
+  `entry_id` int(11) NOT NULL AUTO_INCREMENT,
+  `valid_from` datetime NOT NULL COMMENT 'UTC+0',
+  `valid_to` datetime NOT NULL COMMENT 'UTC+0',
+  `value_exc_vat` decimal(7, 4) NOT NULL COMMENT 'GBp',
+  `value_inc_vat` decimal(7, 4) NOT NULL COMMENT 'GBp',
+  PRIMARY KEY (`entry_id`)
 );
+
 CREATE TABLE `ElectricConsumption` (
-        `entry_id` INT NOT NULL AUTO_INCREMENT,
-        `interval_start` DATETIME NOT NULL COMMENT 'UTC+0',
-        `interval_end` DATETIME NOT NULL COMMENT 'UTC+0',
-        `consumption` DECIMAL(5,3) NOT NULL COMMENT 'kWh',
-        PRIMARY KEY (`entry_id`)
+  `entry_id` int(11) NOT NULL AUTO_INCREMENT,
+  `interval_start` datetime NOT NULL COMMENT 'UTC+0',
+  `interval_end` datetime NOT NULL COMMENT 'UTC+0',
+  `consumption` decimal(5, 3) NOT NULL COMMENT 'kWh',
+  PRIMARY KEY (`entry_id`)
 );
+
 CREATE TABLE `StandingCharges` (
-        `entry_id` INT NOT NULL AUTO_INCREMENT,
-        `date` DATE NOT NULL,
-        `value_exc_vat` DECIMAL(7,4) NOT NULL COMMENT 'GBp',
-        `value_inc_vat` DECIMAL(7,4) NOT NULL COMMENT 'GBp',
-        PRIMARY KEY (`entry_id`)
+  `entry_id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `value_exc_vat` decimal(7, 4) NOT NULL COMMENT 'GBp',
+  `value_inc_vat` decimal(7, 4) NOT NULL COMMENT 'GBp',
+  PRIMARY KEY (`entry_id`)
 );
