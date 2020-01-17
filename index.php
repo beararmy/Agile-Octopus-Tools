4f591320e797eee4980b8070adce638a0b9f4fc4
beararmy <stef@bear.army>
2020-01-17 11:34:02 +0000

    bit more tidy up on main webpage

--- index.php
+++ index.php
@@ -1,53 +1,29 @@
 <?php
 
-#echo "<h2>You probably want the nice page <a href=index2.php>HERE</a>.<br></h2>";
+// echo "<h2>You probably want the nice page <a href=index2.php>HERE</a>.<br></h2>";
 
-require './functions.php'; # Load functions
-require './secrets.php'; # Load secrets
+require './functions.php'; // Load functions
+require './secrets.php'; // Load secrets
 
-# Test: function TestOctopusLogin
-#$call_url = $base . $emeter . $elec_mpan . "/";
-#$status_octopus = TestOctopusLogin($api_key, $call_url);
-#echo $status_octopus;
-
-# Test: function TestMySQLLogin
-#$status_octopus = TestMySQLLogin();
-#echo $status_octopus;
-
-# Test: function GetUsage
-#print("<pre>" . print_r(GetUsage($api_key), true) . "</pre>");
-
-# Test: function GetUpcomingPrices
-#print("<pre>" . print_r(GetUpcomingPrices($api_key), true) . "</pre>");
-
-# Test: function InsertUpcomingPrices
-#$pricesArray = GetUpcomingPrices($api_key);
-#InsertUpcomingPrices($pricesArray);
-
-# Test: function InsertRecentUsage
-#$pricesArray = GetUsage($api_key);
-#InsertRecentUsage($pricesArray);
-
-#http://bike.bear.army/azure-function/functions.php?automated=yes
 if (isset($_GET['automated'])) {
-    #echo "Doing automated stuff";
+    // This is automated, called periodically with cronjob to pull in new data
     try {
-        # Do some checks
+        // Do some checks
         $call_url = $base . $emeter . $elec_mpan . "/";
         $status_octopus = TestOctopusLogin($api_key, $call_url);
         $status_mysql = TestMySQLLogin();
 
-        # Update tomorrow's prices (expected 16:00 UTC/0)
-        $pricesArray = GetUpcomingPrices($api_key);
+        // Update tomorrow's prices (expected 16:00 UTC/0)
+        $pricesArray = GetUpcomingPrices();
         InsertUpcomingPrices($pricesArray);
 
-        # Update the most recent usage stats
-        $pricesArray = GetUsage($api_key);
+        // Update the most recent usage stats
+        $pricesArray = GetUsage();
         InsertRecentUsage($pricesArray);
 
-        # Update the standing charges for the day
+        // Update the standing charges for the day
         $date = date("Y-m-d");
-        $pricesArray = GetStandingCharge($tariff_code);
+        $pricesArray = GetStandingCharge();
         InsertStandingCharge($pricesArray, $date);
     } catch (\Throwable $th) {
         throw $th;
@@ -59,24 +35,45 @@ if (isset($_GET['automated'])) {
     echo "Statuses: Octopus is <b>$status_octopus</b>, MySQL is <b>$status_mysql</b>";
 }
 
-# Interactive parts
-echo "<h2>GetTotalCost</h2>";
-$start_date = "2020-01-03";
-$end_date = "2020-01-05";
-print("<pre>" . print_r(GetTotalCost($start_date, $end_date), true) . "</pre>");
+// Below are many examples of working code and how you may use each function
 
-echo "<h3> The current rate is:</h3>";
-print("<pre>" . print_r(GetCurrentRate(), true) . "</pre>");
-#echo GetCurrentRate()['current_rate_per_kWh'];
+// Test: function TestOctopusLogin
+// $call_url = $base . $emeter . $elec_mpan . "/";
+// $status_octopus = TestOctopusLogin($api_key, $call_url);
+// echo $status_octopus;
 
-echo "<h2>The highest 30 min periods today are:</h2>";
-print("<pre>" . print_r(GetHighestRate('10'), true) . "</pre>");
+// Test: function TestMySQLLogin
+// $status_octopus = TestMySQLLogin();
+// echo $status_octopus;
 
-#echo "<h3> </h3>";
-#GetHighestRate("3");
+// Test: function GetUsage
+// print("<pre>" . print_r(GetUsage(), true) . "</pre>");
 
-echo "<h2>GetUpcomingPrices</h2>";
-print("<pre>" . print_r(GetUpcomingPrices($api_key)['results'], true) . "</pre>");
+// Test: function GetUpcomingPrices
+// print("<pre>" . print_r(GetUpcomingPrices($api_key), true) . "</pre>");
 
-echo "<h2>GetDaysRatesFromDB</h2>";
-print("<pre>" . print_r(GetDaysRatesFromDB(), true) . "</pre>");
+// Test: function InsertUpcomingPrices
+// $pricesArray = GetUpcomingPrices($api_key);
+// InsertUpcomingPrices($pricesArray);
+
+// Test: function InsertRecentUsage
+// $pricesArray = GetUsage($api_key);
+// InsertRecentUsage($pricesArray);
+
+// Test: function GetTotalCost
+// $start_date = "2020-01-03";
+// $end_date = "2020-01-05";
+// print("<pre>" . print_r(GetTotalCost($start_date, $end_date), true) . "</pre>");
+
+// Test: function GetCurrentRate
+// print("<pre>" . print_r(GetCurrentRate(), true) . "</pre>");
+// echo GetCurrentRate()['current_rate_per_kWh'];
+
+// Test: function GetHighestRate
+// print("<pre>" . print_r(GetHighestRate('10'), true) . "</pre>");
+
+// Test: function GetUpcomingPrices
+// print("<pre>" . print_r(GetUpcomingPrices($api_key)['results'], true) . "</pre>");
+
+// Test: function GetDaysRatesFromDB
+// print("<pre>" . print_r(GetDaysRatesFromDB(), true) . "</pre>");
