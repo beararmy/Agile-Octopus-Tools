cce9dd67dc8aa02c71688ae356d445f735b25f98
Stef Harrington-Palmer <stef@bear.army>
2020-06-19 19:26:41 +0100

    ENV based secrets

--- secrets.php
+++ secrets.php
@@ -1,10 +1,10 @@
 <?php
 
 // DB (mysql/mariadb) config
-$db_servername_8459 = "";
-$db_username_2734   = "";
-$db_password_1924   = "";
-$db_name_9781       = "";
+$db_servername_8459 = getenv("db_servername_8459");
+$db_username_2734   = getenv("db_username_2734");
+$db_password_1924   = getenv("db_password_1924");
+$db_name_9781       = getenv("db_name_9781");
 $db_tablename_9834  = "ElectricPrices";
 $db_tablename_9833  = "ElectricConsumption";
 $db_tablename_7439  = "StandingCharges";
@@ -12,9 +12,9 @@ $db_tablename_7439  = "StandingCharges";
 // Octopus config
 $short_tariff_code  = "AGILE-18-02-21";
 $tariff_code        = "E-1R-AGILE-18-02-21-A";
-$api_key            = "";
-$elec_mpan          = "";
-$elec_serial        = "";
+$api_key            = getenv("api_key");
+$elec_mpan          = getenv("elec_mpan");
+$elec_serial        = getenv("elec_serial");
 $agile_reset_time_utc0 = "16:00";
 
 // Octopus API endpoints
