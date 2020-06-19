3f9ef759b08b67a71872b87518c9502d23ea0ec9
Stef Harrington-Palmer <stef@bear.army>
2020-06-19 19:18:12 +0100

    blank secrets, for env settings

---
+++ secrets.php
@@ -0,0 +1,24 @@
+<?php
+
+// DB (mysql/mariadb) config
+$db_servername_8459 = "";
+$db_username_2734   = "";
+$db_password_1924   = "";
+$db_name_9781       = "";
+$db_tablename_9834  = "ElectricPrices";
+$db_tablename_9833  = "ElectricConsumption";
+$db_tablename_7439  = "StandingCharges";
+
+// Octopus config
+$short_tariff_code  = "AGILE-18-02-21";
+$tariff_code        = "E-1R-AGILE-18-02-21-A";
+$api_key            = "";
+$elec_mpan          = "";
+$elec_serial        = "";
+$agile_reset_time_utc0 = "16:00";
+
+// Octopus API endpoints
+$base = "https://api.octopus.energy/v1/";
+$products = "products/";
+$emeter = "electricity-meter-points/";
+$upcomingagilerate = "products/AGILE-18-02-21/electricity-tariffs/E-1R-AGILE-18-02-21-A/standard-unit-rates/";
