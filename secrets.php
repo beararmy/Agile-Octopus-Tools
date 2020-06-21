d0c58ed00568e7eb060dabd12d77c00b209fbbe9
Stef Harrington-Palmer <stef@bear.army>
2020-06-21 10:02:02 +0100

    best practice for caps

--- secrets.php
+++ secrets.php
@@ -1,10 +1,10 @@
 <?php
 
 // DB (mysql/mariadb) config
-$db_servername_8459 = getenv("db_servername_8459");
-$db_username_2734   = getenv("db_username_2734");
-$db_password_1924   = getenv("db_password_1924");
-$db_name_9781       = getenv("db_name_9781");
+$db_servername_8459 = getenv("DB_SERVERNAME_8459");
+$db_username_2734   = getenv("DB_USERNAME_2734");
+$db_password_1924   = getenv("DB_PASSWORD_1924");
+$db_name_9781       = getenv("DB_NAME_9781");
 $db_tablename_9834  = "ElectricPrices";
 $db_tablename_9833  = "ElectricConsumption";
 $db_tablename_7439  = "StandingCharges";
@@ -13,8 +13,8 @@ $db_tablename_7439  = "StandingCharges";
 $short_tariff_code  = "AGILE-18-02-21";
 $tariff_code        = "E-1R-AGILE-18-02-21-A";
 $api_key            = getenv("api_key");
-$elec_mpan          = getenv("elec_mpan");
-$elec_serial        = getenv("elec_serial");
+$elec_mpan          = getenv("ELEC_MPAN");
+$elec_serial        = getenv("ELEC_SERIAL");
 $agile_reset_time_utc0 = "16:00";
 
 // Octopus API endpoints
