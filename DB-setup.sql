d630a7aceb40a8f9f5c7f4c285df343bd2f71096
Stef Harrington-Palmer <stef@bear.army>
2020-06-21 11:15:31 +0100

    tidy up repo

--- DB-setup.sql
+++ DB-setup.sql
@@ -1,13 +1,8 @@
 CREATE DATABASE `AgileOctopus`;
-
 CREATE USER 'OctopusUser' @'localhost' IDENTIFIED BY 'xxxxA password better than this.xxxx';
-
 GRANT ALL PRIVILEGES ON AgileOctopus.* TO 'OctopusUser' @'localhost';
-
 FLUSH PRIVILEGES;
-
 USE 'AgileOctopus';
-
 CREATE TABLE `ElectricPrices` (
   `entry_id` int(11) NOT NULL AUTO_INCREMENT,
   `valid_from` datetime NOT NULL COMMENT 'UTC+0',
@@ -16,7 +11,6 @@ CREATE TABLE `ElectricPrices` (
   `value_inc_vat` decimal(7, 4) NOT NULL COMMENT 'GBp',
   PRIMARY KEY (`entry_id`)
 );
-
 CREATE TABLE `ElectricConsumption` (
   `entry_id` int(11) NOT NULL AUTO_INCREMENT,
   `interval_start` datetime NOT NULL COMMENT 'UTC+0',
@@ -24,7 +18,6 @@ CREATE TABLE `ElectricConsumption` (
   `consumption` decimal(5, 3) NOT NULL COMMENT 'kWh',
   PRIMARY KEY (`entry_id`)
 );
-
 CREATE TABLE `StandingCharges` (
   `entry_id` int(11) NOT NULL AUTO_INCREMENT,
   `date` date NOT NULL,
